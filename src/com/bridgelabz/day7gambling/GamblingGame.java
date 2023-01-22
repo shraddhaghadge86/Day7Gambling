e09e8db86eaa1a7c510e2976d1c32c0f1c5443ba
shraddhaghadge86 <shraddhaghadge86@gmail.com>
2023-01-22 13:25:48 +0530

    Added: Playing for twenty days

--- src/com/bridgelabz/day7gambling/GamblingGame.java
+++ src/com/bridgelabz/day7gambling/GamblingGame.java
@@ -7,23 +7,30 @@ public class GamblingGame {
         int money = 100;
         int playCount = 0;
         int wonCount = 0;
+        int lossCount = 0;
         Random random = new Random();
-        while(money != 151&&money != 49){
+        int day = 1;
+        int forDay = 20;
+        for (int i = day; i <= forDay; i++){
             playCount++;
-            int bet = random.nextInt(2);
-            if(bet == 1){
-                money = money+1;
-                wonCount++;
-            }else{
-                money = money-1;
-                System.out.println("Remaining Money Is :"+money);
+            int bet = random.nextInt(3);
+            switch (bet){
+                case 1:
+                    money = money+1;
+                    wonCount++;
+                    System.out.println("player current money is "+money);
+                    break;
+                case 2:
+                    money = money-1;
+                    lossCount++;
+                    System.out.println("player loss bet so current money is "+money);
+                    break;
+                default:
+                    System.out.println("player stop Gambling for a day");
             }
+            System.out.println("current day "+ day++);
         }
-        if (money > 150){
-            System.out.println("player win more than its 50 percent of stack");
-        }else {
-            System.out.println("player looses more than its 50 percent of stack");
-        }
+
         System.out.println("No of times Played:"+playCount);
         System.out.println("No Of Times Won:"+wonCount);
         System.out.println("No Of Times Lost:"+(playCount-wonCount));
