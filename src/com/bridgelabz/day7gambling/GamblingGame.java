5acaaef574f864004573e302b8bf8d536b9186e7
shraddhaghadge86 <shraddhaghadge86@gmail.com>
2023-01-22 12:41:04 +0530

    Added: Stake and Bet

--- src/com/bridgelabz/day7gambling/GamblingGame.java
+++ src/com/bridgelabz/day7gambling/GamblingGame.java
@@ -1,4 +1,10 @@
 package com.bridgelabz.day7gambling;
 
 public class GamblingGame {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambling Game");
+        int money=100;
+        int betMoney=1;
+        System.out.println("start with a stake of " + money +" every day and bet " + betMoney +" every game.");
+    }
 }
