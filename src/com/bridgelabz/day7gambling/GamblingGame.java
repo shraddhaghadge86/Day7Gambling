2ab74fd1214b03f2fae5c73b45b19edd9665eaf4
shraddhaghadge86 <shraddhaghadge86@gmail.com>
2023-01-22 12:46:34 +0530

    Added: Win or loss the bet

--- src/com/bridgelabz/day7gambling/GamblingGame.java
+++ src/com/bridgelabz/day7gambling/GamblingGame.java
@@ -1,10 +1,20 @@
 package com.bridgelabz.day7gambling;
 
+import java.util.Random;
+
 public class GamblingGame {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Game");
         int money=100;
         int betMoney=1;
+        Random random = new Random();
         System.out.println("start with a stake of " + money +" every day and bet " + betMoney +" every game.");
+        int bet =random.nextInt(2);
+        System.out.println(bet);
+        if (bet==1){
+            System.out.println("Player Win the bet");
+        }else {
+            System.out.println("Player loss the bet");
+        }
     }
 }
